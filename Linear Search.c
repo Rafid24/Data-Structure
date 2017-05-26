4e4b40ae9b3a38d7baa99d555262563d2a6cad2d
Rafid24 <rafidmahboob@gmail.com>
2017-05-26 11:47:43 +0600

    Add files via upload

---
+++ Linear Search.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter Array Size:");
+    scanf("%d",&n);
+    int arr[n];
+    int i, key, found = 0;
+    int loc=0;
+    printf("Enter Array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Enter Key:");
+    scanf("%d",&key);
+
+    for (i = 0; i < n ; i++)
+    {
+        if (key == arr[i] )
+        {
+            found ++;
+            break;
+        }
+    }
+    if (found != 0)
+        printf("Element is present in the array\n");
+    else
+        printf("Element is not present in the array\n");
+
+    for (i = 0; i < n ; i++)
+    {
+        if (key == arr[i] )
+        {
+            loc=i;
+
+        }
+    }
+    printf("Location = %d\n",loc);
+
+    for (i = 0; i < n ; i++)
+    {
+        if (key == arr[i] )
+        {
+            found++;
+            //break;
+        }
+    }
+    printf("The number is %d times\n",found);
+    return 0;
+}
