b1fe9104fcd79eb77ac060d5e898b15cf17395c5
Rafid24 <rafidmahboob@gmail.com>
2017-06-04 21:29:48 +0600

    Add files via upload

---
+++ min in array.c
@@ -0,0 +1,21 @@
+p#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter Array Number:");
+    scanf("%d",&n);
+    int arr[n],i,min;
+    printf("Enter Array Elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(i==0)
+            min=arr[i];
+        if(arr[i]<min)
+        {
+            min=arr[i];
+        }
+    }
+    printf("Minimun=%d",min);
+}
+
